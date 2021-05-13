b68e60af80bc3e9642deefe0db593da9998ef0c2
HET <team@pledra.com>
2021-05-13 16:57:12 +0530

    [MIG] rebase product configurator module.

--- product_configurator/static/js/data_manager.js
+++ product_configurator/static/js/data_manager.js
@@ -8,9 +8,13 @@ odoo.define("product_configurator.DataManager", function (require) {
          * Disable view caching for product.configurator model as it has a very
          * heavy reliance on the fields_view_get method to generate and update
          * dynamic content.
+         *
+         * @param {Object} params see load_view documentation
+         * @param {Object} options see load_view documentation
+         * @returns the load_views expected result
          */
         load_views: function (params, options) {
-            if (params.context.view_cache == false) {
+            if (params.context.view_cache === false) {
                 this.invalidate();
             }
             return this._super(params, options);
