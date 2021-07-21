0d333a6b34964ff39c2c705ff5149fc0fdb28aa9
SLIMBOOK TEAM <dev@slimbook.es>
2021-07-21 22:33:52 +0200

    Update benchmark_nodejs.sh

--- benchmark_nodejs.sh
+++ benchmark_nodejs.sh
@@ -18,10 +18,12 @@ tar xf v14.17.3.tar.gz
 cd node-14.17.3 
 ./configure  
 
-start_time="$(date -u +%s)" 
-make -ki -j $threads 2>&1 end_time="$(date -u +%s)"  
+start_time="$(date -u +%s)"
+make -s -j $threads 2>&1 
+end_time="$(date -u +%s)"
 
-elapsed="$(($end_time-$start_time))"  
+elapsed="$(($end_time-$start_time))"
+
 
 echo "Total of $elapsed seconds elapsed for process"
 echo "Your cpu: $cpu"
