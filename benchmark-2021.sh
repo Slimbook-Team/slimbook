f2a076387e56fa9eef7cf35239bbb97ce8fe4628
SLIMBOOK TEAM <dev@slimbook.es>
2021-07-21 10:17:27 +0200

    Create benchmark-2021.sh

--- benchmark-2021.sh
+++ benchmark-2021.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 threads=$(lscpu | egrep 'CPU\(s\)\:' | cut -d ':' -f 2)
 
+cpu=$(cat /proc/cpuinfo | grep 'model name' | head -1 | cut -d ':' -f2)
+
 wget https://github.com/nodejs/node/archive/refs/tags/v14.17.3.tar.gz 
 tar xf v14.17.3.tar.gz 
 cd node-14.17.3 
@@ -12,3 +14,4 @@ make -s -j $threads 2>&1 end_time="$(date -u +%s)"
 elapsed="$(($end_time-$start_time))"  
 
 echo "Total of $elapsed seconds elapsed for process"
+echo "Your cpu: $cpu"
