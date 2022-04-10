06597799844facd1623f43b3286d7ab9ab8e2e81
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-04-10 20:56:56 +0200

    change node version

--- benchmark_nodejs.sh
+++ benchmark_nodejs.sh
@@ -8,26 +8,54 @@
 # wait 10 minutes (or more)
 # share your result with us, twitter: @SlimbookES
 
+clean_test(){
+    if [[ -d "/tmp/node-${VERSION}" ]]
+    then
+        rm -rf "/tmp/node-${VERSION}"
+    fi
+    if [[ -f "/tmp/v${VERSION}.tar.gz" ]]
+    then
+        rm -f "v${VERSION}.tar.gz"
+    fi
+}
 
-threads=$(lscpu | egrep '^CPU\(s\)\:' | cut -d ':' -f 2)
 
-cpu=$(cat /proc/cpuinfo | grep 'model name' | head -1 | cut -d ':' -f2)
+VERSION='14.19.1'
 
-cd /tmp
-rm -f v14.17.3.tar.gz
-rm -rf node-14.17.3
-wget https://github.com/nodejs/node/archive/refs/tags/v14.17.3.tar.gz 
-tar xf v14.17.3.tar.gz 
-cd node-14.17.3 
-./configure  
+THREADS=$(lscpu | grep -E '^CPU\(s\)\:' | awk '{print $2}')
+CPU=$(grep 'model name' /proc/cpuinfo | head -1 | cut -d ':' -f2)
+CURDIR="$PWD"
 
-start_time="$(date -u +%s)"
-make -s -j $threads 2>&1 
-end_time="$(date -u +%s)"
+cd /tmp || exit
+# Clean before test
+clean_test
 
-elapsed="$(($end_time-$start_time))"
-h=$(($elapsed/3600))
-m=$((($elapsed%3600)/60))
-s=$(($elapsed%60))
+wget "https://github.com/nodejs/node/archive/refs/tags/v${VERSION}.tar.gz"
+rs=$?
+if [[ $rs -ne 0 || ! -f "v${VERSION}.tar.gz" ]]
+then
+    echo "Can not download"
+    exit 1
+fi
 
-printf "\nCPU benchmark finished. \nCPU:$cpu \nTotal time (hours:minutes:seconds): %02d:%02d:%02d \n" $h $m $s
+tar xf "v${VERSION}.tar.gz"
+cd "node-${VERSION}" || exit
+
+./configure
+
+START_TIME="$(date -u +%s)"
+make -s -j "$THREADS" 2>&1
+END_TIME="$(date -u +%s)"
+
+ELAPSED="$((END_TIME - START_TIME))"
+h=$((ELAPSED / 3600))
+m=$(((ELAPSED % 3600) / 60))
+s=$((ELAPSED % 60))
+
+printf "\n=== CPU benchmark finished ==="
+printf "\nCPU: %s" "$CPU"
+printf "\nTotal time (hours:minutes:seconds): %02d:%02d:%02d \n" $h $m $s
+
+# Clean after test
+clean_test
+cd "$CURDIR"
