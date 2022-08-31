d184b092a388dad511635be1fa00ac8de6ab58f9
Slimbook Team <dev@slimbook.es>
2022-08-31 16:09:39 +0200

    Update executive_edid.sh

--- executive_edid.sh
+++ executive_edid.sh
@@ -7,6 +7,8 @@
 # Exec:
 # sudo bash executive_edid.sh
 
+DISTRO=$(lsb_release -is)
+
 cd /tmp || exit # && echo "We are now in $PWD"
 wget https://raw.githubusercontent.com/slimbook/slimbook/master/assets/executive_edid_mod90hz.bin
 sudo mkdir /lib/firmware/edid/
@@ -21,4 +23,14 @@ sudo chmod +x /etc/initramfs-tools/hooks/edid
 
 sudo update-initramfs -u
 sudo update-grub
+
+#fedora start (y algunos arch)
+if [ $DISTRO = "Fedora" ] || [ $DISTRO = "Manjaro" ]; then
+  wget https://raw.githubusercontent.com/slimbook/slimbook/master/assets/executive_edid.conf
+  sudo cp /tmp/executive_edid.conf /etc/dracut.conf.d/executive_edid.conf
+  sudo dracut -f
+  sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
+fi
+#fedora end
+
 echo "Finalizado, reinicie el ordenador para aplicar los cambios"
