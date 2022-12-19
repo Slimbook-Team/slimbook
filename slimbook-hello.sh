c710504cf076bc3f776ec7500e510f2e2f6874fd
Slimbook <dev@slimbook.es>
2022-12-19 18:22:09 +0100

    Create slimbook-hello.sh

---
+++ slimbook-hello.sh
@@ -0,0 +1,19 @@
+printf "
+ ███████ ██      ██ ███    ███ ██████   ███████   ███████  ██   ██ 
+██       ██      ██ ████  ████ ██   ██ ██     ██ ██     ██ ██  ██  
+ ██████  ██      ██ ██ ████ ██ ██████  ██     ██ ██     ██ █████   
+      ██ ██      ██ ██  ██  ██ ██   ██ ██     ██ ██     ██ ██  ██  
+███████  ███████ ██ ██      ██ ██████   ███████   ███████  ██   ██                                              
+"
+lsb_release -ar 2>/dev/null
+echo "Kernel version: $(uname -r)" 
+echo "Session type:   $XDG_SESSION_TYPE"
+
+echo	
+
+free -h
+
+echo
+
+lsblk /dev/sd* 2>/dev/null
+lsblk /dev/nvme* 2>/dev/null
